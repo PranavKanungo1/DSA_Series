b5f0237e2049f4b7b41d5121b273ae86be8619f3
PranavKanungo1 <pranavkanungo19@gmail.com>
2026-07-27 12:23:45 +0530

    maximum subarray sum

---
+++ bruteforce.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main(){
+    int n = 7;
+    int arr[7] = {3,-4, 5, 4, -1, 7, -8};
+    int maxSum = INT_MIN;
+
+    for(int st = 0; st < n; st++){
+        int currSum = 0;
+        for(int end = st; end < n; end++){
+            currSum += arr[end];
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    cout << "Maximum Subarray Sum is: " << maxSum << endl;
+    return 0;
+}
